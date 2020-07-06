6f966365afcf4e9019e3ca413cf78aa1bb6ab302
Magdy_mazrou@yahoo.com <Mego2010@@>
2020-07-06 15:27:42 +0200

    Update phone-number.js

--- BackEnd/common/models/phone-number.js
+++ BackEnd/common/models/phone-number.js
@@ -4,9 +4,8 @@ module.exports = function (Phonenumber) {
     Phonenumber.observe('after save', function (ctx, next) {
         var massage = "E-Delivery\n"+
         "fb.com/EDelivery2020\n" +
-        "كود العميل : "+ ctx.instance["customerId"] +"\n"+
-        "مرحبا بكم في متابعة خدمات التوصيل 01224120900";
-        var res = encodeURI('https://smsmisr.com/api/webapi/?username=7TLgq36R&password=3fMMaB0OMX&language=2&sender=D Services&mobile=2'+ctx.instance["Phone_Number"]+'&message='+massage); 
+        "كود العميل : "+ ctx.instance["customerId"] ;
+        var res = encodeURI('https://smsmisr.com/api/webapi/?username=######&password=######&language=2&sender=D Services&mobile=2'+ctx.instance["Phone_Number"]+'&message='+massage); 
         var options = {
             'method': 'POST',
             'url': res,
